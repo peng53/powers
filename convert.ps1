a2ef02a090ac9afe4b5e7d27ac6bf16ffda6a82e
Perry Eng <eng.perry@gmail.com>
2022-04-09 09:08:30 -0500

    convert.ps1 actually converts now

--- convert.ps1
+++ convert.ps1
@@ -1,15 +1,25 @@
 ﻿#Requires –Version 2.0
 Add-Type -AssemblyName system.drawing
-#$imageFormat = “System.Drawing.Imaging.ImageFormat” -as [type]
-$outdir = "r:/outd"
-$outpre = "page"
-#$image.Save($saveFile, $imageFormat::jpeg)
-foreach ($a in $args){
-    if (-not $a.EndsWith('.tif')){ continue }
-    $image = [drawing.image]::FromFile($a)
-    $c = $image.GetFrameCount([System.Drawing.Imaging.FrameDimension]::Page);
-    for ($i = 0; $i -lt $c; $i++){
-        $image.SelectActiveFrame([System.Drawing.Imaging.FrameDimension]::Page, $i)
-        $image.Save('{0}/{1}_{2}.tif' -f ($outdir,$outpre,$i))
-    }
+$imageFormat = “System.Drawing.Imaging.ImageFormat” -as [type]
+$fmts = @('bmp','gif','jpeg','png','tiff')
+foreach ($c in $fmts){
+	Write-Host $c
 }
+$outfmt = Read-Host 'Output format'
+if ($fmts -contains $outfmt){
+	foreach ($a in $args){
+		$t = $a+'.'+$outfmt
+		Write-Host $a
+		if (!($a.EndsWith('.'+$outfmt)) -or !(Test-Path -path $t)){
+			$image = [drawing.image]::FromFile($a)
+			$image.Save($t, $imageFormat::$outfmt)
+			Write-Host "NEW: $t"
+		} else{
+			Write-Host "ERR: $t"
+		}
+	}
+	Write-Host 'DONE'
+} else {
+	Write-Host "$outfmt is not valid choice."
+}
+Read-Host
