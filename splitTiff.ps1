5981ffbd33621d5e09dbd73f0bfd8c4d2382d558
Perry Eng <eng.perry@gmail.com>
2022-04-09 09:07:08 -0500

    Initial commit splitTiff.ps1. Changing convert.ps1 to actually convert

---
+++ splitTiff.ps1
@@ -0,0 +1,24 @@
+﻿#Requires –Version 2.0
+Add-Type -AssemblyName system.drawing
+#$imageFormat = “System.Drawing.Imaging.ImageFormat” -as [type]
+
+Add-Type -AssemblyName System.Windows.Forms
+$saveFileDialog = New-Object System.Windows.Forms.SaveFileDialog
+
+#$image.Save($saveFile, $imageFormat::jpeg)
+foreach ($a in $args){
+    $saveFileDialog.FileName = ''
+    $saveFileDialog.title = "Output files for $a"
+    $saveFileDialog.initialDirectory = split-path $a
+    $saveFileDialog.ShowDialog()
+    $outfile_pre = $saveFileDialog.FileName
+    if ($outfile_pre -ne ''){
+        $image = [drawing.image]::FromFile($a)
+        $c = $image.GetFrameCount([System.Drawing.Imaging.FrameDimension]::Page);
+        for ($i = 0; $i -lt $c; $i++){
+            $image.SelectActiveFrame([System.Drawing.Imaging.FrameDimension]::Page, $i) | out-null
+            $image.Save($outfile_pre + '_' + $i + '.tif')
+            Write-Host "File write ${outfile_pre}_${i}.tif"
+        }
+    }
+}
