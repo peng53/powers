2d2c30d1e1dba6d0131883c3152a3034d64e888b
Perry Eng <eng.perry@gmail.com>
2022-04-15 22:18:53 -0500

    Included cases where: input file isn't a tif, intended output folder already exists

--- splitTiff2Dir.ps1
+++ splitTiff2Dir.ps1
@@ -1,14 +1,23 @@
 ﻿foreach ($a in $args){
-    $f = Get-Item $a
-    $od = New-Item -Path $f.Directory -Name $f.BaseName -ItemType Directory
-    try { $img = [drawing.image]::FromFile($a) }
-    catch {
-        Add-Type -AssemblyName system.drawing
-        $img = [drawing.image]::FromFile($a)
-    }
-    $c = $img.GetFrameCount([System.Drawing.Imaging.FrameDimension]::Page)
-    for ($i = 0; $i -lt $c; $i++){
-        $img.SelectActiveFrame([System.Drawing.Imaging.FrameDimension]::Page, $i) | out-null
-        $img.Save($od.toString() + '/' + $f.BaseName + '_' + $i + '.tif')
+    if ($a.EndsWith('.tif')){
+        $f = Get-Item $a
+        $od = '{0}\{1}' -f ($f.Directory,$f.BaseName)
+        if (!(Test-Path $od)){
+            New-Item -Path $od -ItemType Directory | out-null
+            try { $img = [drawing.image]::FromFile($a) }
+            catch {
+                Add-Type -AssemblyName system.drawing
+                $img = [drawing.image]::FromFile($a)
+            }
+            $c = $img.GetFrameCount([System.Drawing.Imaging.FrameDimension]::Page)
+            for ($i = 0; $i -lt $c; $i++){
+                $img.SelectActiveFrame([System.Drawing.Imaging.FrameDimension]::Page, $i) | out-null
+                $img.Save($od.toString() + '/' + $f.BaseName + '_' + $i + '.tif')
+            }
+        } else {
+            Write-Host 'Folder' $od 'already exists'
+        }
+    } else {
+        Write-Host 'Skipping non-tif' $a
     }
 }
