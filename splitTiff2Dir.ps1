b8c95e89cea5bde12ad5a4f9315ca7a4c09a7433
Perry Eng <eng.perry@gmail.com>
2022-04-15 21:42:04 -0500

    Inital add splitTiff2Dir.ps1 Instead of selecting an output directory, this one makes a directory with the same name as the tif.

---
+++ splitTiff2Dir.ps1
@@ -0,0 +1,14 @@
+﻿foreach ($a in $args){
+    $f = Get-Item $a
+    $od = New-Item -Path $f.Directory -Name $f.BaseName -ItemType Directory
+    try { $img = [drawing.image]::FromFile($a) }
+    catch {
+        Add-Type -AssemblyName system.drawing
+        $img = [drawing.image]::FromFile($a)
+    }
+    $c = $img.GetFrameCount([System.Drawing.Imaging.FrameDimension]::Page)
+    for ($i = 0; $i -lt $c; $i++){
+        $img.SelectActiveFrame([System.Drawing.Imaging.FrameDimension]::Page, $i) | out-null
+        $img.Save($od.toString() + '/' + $f.BaseName + '_' + $i + '.tif')
+    }
+}
