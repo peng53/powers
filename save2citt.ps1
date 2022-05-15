002c815a01bd882686caa2cd748afbcd95643ec1
Perry Eng <eng.perry@gmail.com>
2022-05-15 17:34:31 -0500

    Intial add save2citt.ps1 Saves all layers of a picture to tiff citt1.6 Messy because not sure about powershell

---
+++ save2citt.ps1
@@ -0,0 +1,14 @@
+﻿[Reflection.Assembly]::LoadWithPartialName(“System.Windows.Forms”);
+#$img = New-Object System.Drawing.Bitmap c:/users/lm/pictures/multi.tif.bmp
+$img = New-Object System.Drawing.Bitmap c:/users/lm/pictures/multilzw.tif
+$myenc =  [System.Drawing.Imaging.Encoder]::Compression
+$encoderParams = New-Object System.Drawing.Imaging.EncoderParameters(1)
+$encoderParams.Param[0] = New-Object System.Drawing.Imaging.EncoderParameter($myenc, 3)
+$myImageCodecInfo = [System.Drawing.Imaging.ImageCodecInfo]::GetImageEncoders()|where {$_.MimeType -eq 'image/tiff'}
+$c = $img.GetFrameCount([System.Drawing.Imaging.FrameDimension]::Page)
+for ($i = 0; $i -lt $c; $i++){
+    $img.SelectActiveFrame([System.Drawing.Imaging.FrameDimension]::Page, $i) | out-null
+    $img.Save('r:/lzw_' + $i + '.tif')
+}
+#$img.SelectActiveFrame([System.Drawing.Imaging.FrameDimension]::Page, $i) | out-null
+#$img.save('R:\testsave.tif',$myImageCodecInfo,$($encoderParams))
