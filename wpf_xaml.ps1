93280827e3e2f826e2c2cc38e1f05c48418fa79a
Perry Eng <eng.perry@gmail.com>
2022-08-06 22:26:18 -0500

    Inital add report_make.xaml Move XAML code to seperate file. Expanded view with more widgets

--- wpf_xaml.ps1
+++ wpf_xaml.ps1
@@ -1,38 +1,7 @@
 #requires -version 5.1
 add-type -assemblyname PresentationFramework
-[xml]$XAML = @"
-<Window xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation" Width="750" Height="500" Title="wpf_xaml.ps1">
-    <Grid>
-        <Grid.ColumnDefinitions>
-            <ColumnDefinition/>
-        </Grid.ColumnDefinitions>
-        <Grid.RowDefinitions>
-            <RowDefinition Height="Auto"/>
-            <RowDefinition/>
-        </Grid.RowDefinitions>
-        <ToolBarTray HorizontalAlignment="Left" Height="28" Grid.Row="0" VerticalAlignment="Top">
-            <ToolBar HorizontalAlignment="Left" VerticalAlignment="Top">
-                <Button Name="TestButton" Content="Button1" />
-                <Button Name="TestButton2" Content="Button2" />
-                <Label Content="Name" Height="23" VerticalAlignment="Top"/>
-                <TextBox Height="23" Margin="0" TextWrapping="Wrap" Text="[namehere]" VerticalAlignment="Top" Width="120"/>
-            </ToolBar>
-        </ToolBarTray>
-        <ListView Name="listv" Grid.Row="1">
-            <ListView.View>
-                <GridView>
-                    <GridViewColumn Header="Ref" Width="120" DisplayMemberBinding="{Binding Path=Id}"/>
-                    <GridViewColumn Header="Type" Width="100" DisplayMemberBinding="{Binding Path=Membership}"/>
-                    <GridViewColumn Header="Front" Width="250" DisplayMemberBinding="{Binding Path=FirstName}"/>
-                    <GridViewColumn Header="Back" Width="250" DisplayMemberBinding="{Binding Path=LastName}"/>
-                </GridView>
-            </ListView.View>
-        </ListView>
 
-    </Grid>
-</Window> 
-"@
-
+[xml]$XAML = Get-Content report_make.xaml
 $Form=[Windows.Markup.XamlReader]::Load([System.Xml.XmlNodeReader]::new($XAML))
 $listv = $Form.FindName('listv')
 <#for ($i=0;$i -lt $listv.Items.Count;++$i){
