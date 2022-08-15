43a868fe84aaa1c1810027d9b9addf6c029e802d
Perry Eng <eng.perry@gmail.com>
2022-08-15 07:32:19 -0500

    Added remove button. This uses the new member variable 'IsSelected' of MyRow. Also added Create Row button, only uses the widget's current text; not sure how to bind that yet

--- wpf_xaml.ps1
+++ wpf_xaml.ps1
@@ -1,39 +1,56 @@
 #requires -version 5.1
 add-type -assemblyname PresentationFramework
 
-[xml]$XAML = Get-Content report_make.xaml
-$Form=[Windows.Markup.XamlReader]::Load([System.Xml.XmlNodeReader]::new($XAML))
-$listv = $Form.FindName('listv')
-<#for ($i=0;$i -lt $listv.Items.Count;++$i){
-	write-host $listv.Items[$i]
-}#>
 enum MembershipLevel {
-    VIP
-    Agent
-    IronChef
+	ClassF
+	VIP
+	Agent
+	IronChef
 }
 class MyRow {
+	[bool]$IsSelected
 	[int]$Id
 	[MembershipLevel]$Membership
 	[string]$FirstName
 	[string]$LastName
-    [string]ToString(){
-        $s = '{0} {1} is member level {2} with ID# {3}' -f $this.FirstName,$this.LastName,$this.Membership,$this.Id
-        return $s
-    }
+	[string]ToString(){
+		$s = '{0} {1} is member level {2} with ID# {3} and selected is {4}' -f $this.FirstName,$this.LastName,$this.Membership,$this.Id,$this.IsSelected
+		return $s
+	}
 }
-[void]$listv.Items.Add([MyRow]@{Id=10;Membership=0;FirstName='Jack';LastName='Colby'})
-[void]$listv.Items.Add([MyRow]@{Id=55;Membership=2;FirstName='Ismal';LastName='Drino'})
-$button = $form.FindName('TestButton')
-$button.Add_Click({
-    foreach ($item in $listv.Items){
-        write-host $item
-    }
+
+[xml]$XAML = Get-Content report_make.xaml
+$Form=[Windows.Markup.XamlReader]::Load([System.Xml.XmlNodeReader]::new($XAML))
+$myitems = [system.collections.objectmodel.observablecollection[MyRow]]::new()
+$listv = $Form.FindName('listv')
+$listv.ItemsSource = $myitems
+[void]$myitems.Add([MyRow]@{Id=10;Membership=0;FirstName='Jack';LastName='Colby'})
+[void]$myitems.Add([MyRow]@{Id=55;Membership=2;FirstName='Ismal';LastName='Drino'})
+[void]$myitems.Add([MyRow]@{Id=81;Membership=1;FirstName='Tiep';LastName='Simia'})
+[void]$myitems.Add([MyRow]@{Id=526;Membership=2;FirstName='Zenwalther';LastName='Kooperbanetonopp'})
+$removebutton = $form.FindName('RemoveButton')
+$removebutton.Add_Click({
+	for ($c=$myitems.Count-1;$c -ge 0;$c--){
+		if ($myitems[$c].IsSelected){
+			write-host 'Removing at position' $c ':' $myitems[$c]
+			$myitems.RemoveAt($c)
+		}
+	}
 })
-$button2 = $form.FindName('TestButton2')
-$button2.Add_Click({
-    foreach ($item in $listv.SelectedItems){
-        write-host $item
-    }
+Function GetTextboxInput {
+	$id = $Form.FindName('TbId').Text -replace '[^\d]'
+	$first = $Form.FindName('TbFirstName').Text
+	$last = $Form.FindName('TbLastName').Text
+	if ([string]::IsNullOrWhiteSpace($id)){
+		return
+	}
+	return @{Id=[int]$id; FirstName=$first; LastName=$last}
+}
+$createbutton = $form.FindName('CreateRow')
+$createbutton.Add_Click({
+	$row = GetTextboxInput
+	if ($row){
+		$myitems.Add([MyRow]$row)
+	}
 })
 $Form.ShowDialog()
