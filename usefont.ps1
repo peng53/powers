3f485012a925077e56d13e2969506b5fa68405f2
Perry Eng <eng.perry@gmail.com>
2022-12-09 15:56:57 -0600

    Initial add usefont.ps1 Correction: privatefontcollection does work however using picturebox complicates things someone

---
+++ usefont.ps1
@@ -0,0 +1,17 @@
+﻿$x = new-object System.Drawing.Text.PrivateFontCollection
+$x.AddFontFile('R:\GnuMICR.ttf')
+
+$ff = $x.Families[0]
+
+$f = new-object system.drawing.font $ff, 16
+
+$drawing = new-object system.drawing.bitmap 300,300
+$g = [System.Drawing.Graphics]::FromImage($drawing)
+
+$b = new-object System.Drawing.SolidBrush 'black'
+#$g.DrawString('123',$x.Families[0].Name,$b,(new-object system.drawing.rectanglef 0,0,300,300))
+
+$g.DrawString('A1234567890BCD',$f,$b,(new-object system.drawing.rectanglef 0,0,300,300))
+
+$drawing.save('r:/test.png')
+pause
