50dc6c963e470c1df939b1c6ed5b6cdd0b4f65d5
Perry Eng <eng.perry@gmail.com>
2022-12-11 19:40:58 -0600

    Major changes, preview image the main part (as opposed to datagrid) Changed DatagridCheckBox and ComboBox back to custom template- the former is just annoying to use

--- datagrid-custom.ps1
+++ datagrid-custom.ps1
@@ -20,15 +20,45 @@ $dg.ItemsSource = $items
 #$items.Add([DataItem]::new('r:/test.png'))
 #$items.Add([DataItem]@{Filename='r:/test1.png'})
 #$items.Add([DataItem]::new('r:/test1.png'))
-$items.Add([DataItem]@{Filename='r:/test1.png';First=$true;Meta='1234';Type='A'})
+#$items.Add([DataItem]@{Filename='r:/test1.png';First=$true;Meta='1234';Type='A'})
+foreach ($f in (Get-ChildItem C:\Users\lm\Pictures\*.png)){
+    $items.Add([DataItem]@{Filename=$f})
+}
 $testb = $Form.FindName('TestButton')
 $testb.Add_Click({
+    $dg.CancelEdit()
     for ($i=0;$i -lt $items.Count;$i++){
         if ($items[$i].IsSelected){
             write-host $items[$i].Filename $items[$i].Meta $items[$i].First $items[$i].Type
         }
     }
 })
+
+$gridmode = $Form.FindName('GridMode')
+$gridmode.Add_Click({
+    switch ($dg.SelectionUnit){
+        "Cell" {
+            $dg.SelectionUnit = [System.Windows.Controls.DataGridSelectionUnit]::FullRow
+        }
+        "FullRow" {
+            $dg.SelectionUnit = [System.Windows.Controls.DataGridSelectionUnit]::Cell
+        }
+    }
+    <#
+    if ($dg.SelectionUnit -eq [System.Windows.Controls.DataGridSelectionUnit]::Cell){
+        $dg.SelectionUnit = [System.Windows.Controls.DataGridSelectionUnit]::FullRow
+    } else {
+        $dg.SelectionUnit = [System.Windows.Controls.DataGridSelectionUnit]::Cell
+    }#>
+})
+<#
+$dg.Add_BeginningEdit({
+    $dg.SelectionUnit = [System.Windows.Controls.DataGridSelectionUnit]::Cell
+})
+$dg.Add_CellEditEnding({
+    $dg.SelectionUnit = [System.Windows.Controls.DataGridSelectionUnit]::FullRow
+})
+#>
 $previewimg = $Form.FindName('PreviewImg')
 $dg.Add_CurrentCellChanged({
     #write-host $_
