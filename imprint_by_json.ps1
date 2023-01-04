575f39e06a373640eb837f811dbbf84306c42bf1
Perry Eng <eng.perry@gmail.com>
2023-01-04 17:44:12 -0600

    Added drawing steps Font style is also now allowed

--- imprint_by_json.ps1
+++ imprint_by_json.ps1
@@ -5,25 +5,32 @@ $jsonData = convertfrom-json ((Get-Content test_zones.json) -split '`n' -join ''
 
 $fonts = [System.Collections.Generic.Dictionary[[string],[system.drawing.font]]]::new()
 foreach ($z in $jsonData.zones.GetEnumerator()){
-    $font,$size = $z.value['font','fontSize']
-    $name = "$font-$size"
+    $font,$size,$style = $z.value['font','fontSize','fontStyle']
+    $name = "$font-$size-$style"
+    write-host $font
+    $st = [System.Drawing.FontStyle]::Regular
+    switch ($style){
+        'Regular' { $st = [System.Drawing.FontStyle]::Regular }
+        'Bold' { $st = [System.Drawing.FontStyle]::Bold }
+        'Italic' { [System.Drawing.FontStyle]::Italic }
+    }
     if ($font -and -not $fonts.ContainsKey($name)){
-        $fonts.Add($name, [system.drawing.font]::new($font,$size ?? 12))
+        $fonts.Add($name, [system.drawing.font]::new($font,[int]$size ?? 12, $st,[system.drawing.graphicsunit]::Point))
     }
 }
 write-host $fonts
 $brushBlack = [System.Drawing.SolidBrush]::new('black')
 foreach ($p in $jsonData.pages.GetEnumerator()){
     write-host 'create image of size' $jsonData.width 'x' $jsonData.height
-    #$image = [system.drawing.bitmap]::new($jsonData.width,$jsonData.height)
+    $image = [system.drawing.bitmap]::new([int]$jsonData.width,[int]$jsonData.height)
+    $g = [System.Drawing.Graphics]::FromImage($image)
     ## we go zone by zone
     foreach ($z in $p.value.getenumerator()){
         $zname, $zv = $z.name,$z.value
         $zone = $jsonData.zones[$zname]
-        $fontName = '{0}-{1}' -f $zone.font,$zone.fontSize
+        $fontName = '{0}-{1}-{2}' -f $zone.font,$zone.fontSize,$zone.fontStyle
         $font = $fonts[$fontName]
-        $rec = [system.drawing.rectanglef]::new($zone.x,$zone.y,$zone.width,$font.Height+10)
-        #write-host $rec
+        $pt = [system.drawing.pointf]::new($zone.x,$zone.y)
         $stringformat = [system.drawing.stringformat]::new()
         switch ($zone.alignment){
             'center' { $stringformat.alignment = 'Center' }
@@ -31,10 +38,15 @@ foreach ($p in $jsonData.pages.GetEnumerator()){
             'right' { $stringformat.alignment = 'Far' }
         }
         foreach ($v in $zv){
+            $g.DrawString($v,$font,$brushBlack,$pt,$stringformat)
             write-host 'draw string' $v 'with font' $font $brushBlack $rec
-            $rec.y += $font.Height+2
+            $pt.y += $font.Height+2
+
         }
     }
+    $image.save('r:/{0}.png' -f $p.name)
+    $g.dispose()
+    $image.dispose()
 }
 
 $brushBlack.dispose()
