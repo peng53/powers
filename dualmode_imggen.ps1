e17024641e6562373f3d056912eea493f0d5da88
Perry Eng <eng.perry@gmail.com>
2023-06-18 15:28:55 -0500

    Initial add 'dual mode' imggen Which is imprint_by_counter except: * 'dual mode', where there are plans to allow an additional image generation on ignored columns. * use of a split instead of 'condense' with matching page number ** due to the 1 line 1 file output (except dual mode), a filename column was included to direct output * instead of increasing the position by fontSize+2, the zone_specs need to include a dx dy column. ** this means position can be increase (or decrease) by either X or Y each time * simplication of how zones are loaded into memory, ** we don't really need a dedicated dictionary when the original row is still in memory

---
+++ dualmode_imggen.ps1
@@ -0,0 +1,113 @@
+﻿#Requires -Version 7
+add-type -AssemblyName 'system.drawing'
+add-type -AssemblyName 'system.windows.forms'
+
+$f = [system.drawing.font]::new('Arial',12)
+
+#getting specs
+# specs should be csv with header
+# Name,x0,y0,fontSize,font,fontStyle,alignment,color,dx,dy
+$allSpecs = import-csv $psscriptroot/etc/zone_specs_dual.csv
+#getting specs end
+
+$data = import-csv $psscriptroot/etc/test_zones_dual.csv
+# data should have Filename column and all other columns should be in the spec to be captured on file
+# getting cols start
+$ignoreCols = 'Filename'
+$dataCols = $data[0].psobject.Properties.Name | Where-Object {$ignoreCols -notcontains $_}
+$drawCols = [System.Collections.Generic.Dictionary[string,pscustomobject]]::new()
+$fonts = [System.Collections.Generic.Dictionary[[string],[system.drawing.font]]]::new()
+$styles = @{
+    Regular = [System.Drawing.FontStyle]::Regular
+    Bold = [System.Drawing.FontStyle]::Bold
+    Italic = [System.Drawing.FontStyle]::Italic
+}
+foreach ($row in $allSpecs){
+    if ($dataCols.Contains($row.Name)){
+        $drawCols.Add($row.Name, $row)
+        $st = $styles.Regular
+        if ($styles.Containskey($row.fontStyle)){
+            $st = $styles[$row.fontStyle]
+        }
+        $fonts.Add($row.Name, [system.drawing.font]::new($row.Font,[int]$row.fontSize ?? 12, $st,[system.drawing.graphicsunit]::Point))
+    }
+}
+#getting cols end
+$width = 1700
+$height = 600
+$templateFile = 'templateImg.png'
+$template = $null
+if ((Test-Path $templateFile)){
+    $template = [system.drawing.image]::fromfile($templateFile)
+    $width = $template.Width
+    $height = $template.Height
+}
+$folderSelector = [System.Windows.Forms.FolderBrowserDialog]::new()
+$outpath = $null
+$noClobber = $false
+write-host 'Safe mode: ' $noClobber
+if ($folderSelector.ShowDialog() -eq 'OK'){
+    $outpath = $folderSelector.SelectedPath
+} else {
+    exit
+}
+
+#drawing core
+$align = [system.drawing.stringformat]::new()
+foreach ($row in $data){
+    if ($row.Filename.length -le 0){
+        write-host 'Filename column empty'
+        continue
+    }
+    $fn = join-path $outpath $row.Filename
+    if ((Test-path $fn) -and $noClobber){
+        write-host 'File already exists'
+        continue
+    }
+    $frame = $null
+    $g = $null
+    if ($template -eq $null){
+        if ($width -gt 0 -and $height -gt 0){
+            $frame = [system.drawing.bitmap]::new($width,$height)
+            # create blank (white) canvas
+            $g = [system.drawing.graphics]::FromImage($frame)
+            $g.FillRectangle([System.Drawing.Brushes]::White,[system.drawing.rectangle]::new(0,0,$width,$height))
+        }
+    } else {
+        $frame = $template.clone([system.drawing.rectangle]::new(0,0,$width,$height),[System.Drawing.Imaging.PixelFormat]::Format16bppRgb555)
+        $g = [system.drawing.graphics]::FromImage($frame)
+    }
+    if ($frame -eq $null -or $g -eq $null){
+        write-host 'Failed to create image or graphic'
+        break
+    }
+    foreach ($col in $drawCols.GetEnumerator()){
+        if (($row.($col.Key)).Length -lt 1){
+            continue
+        }
+        $vals = ($row.($col.Key)).split('|')
+        $pos = [system.drawing.pointf]::new(($col.value).x0, ($col.value).y0)
+        switch (($col.value).alignment){
+            'center' {$align.alignment = 'Center'}
+            'left' {$align.alignment = 'Near'}
+            'right' {$align.alignment = 'Far'} 
+        } 
+        foreach ($v in $vals){
+            $g.drawstring(
+                $v,
+                $fonts[$col.Key],
+                [system.drawing.brushes]::($col.Value.Color),
+                $pos,
+                $align
+            )
+            $pos.x += ($col.Value).dx
+            $pos.y += ($col.Value).dy
+        }
+    }
+    $frame.save($fn)
+    $g.Dispose()
+    $frame.Dispose()
+}
+if ($template){
+    $template.Dispose()
+}
